9e6137bac7a671828fc6e1162d3909cb607936e4
Snake <snake85@list.ru>
2023-10-01 19:51:59 -0700

    qwe

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY =  os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.getenv(('ALLOWED_HOSTS').split(), default=' ')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'yakitty.ddns.net'] # os.getenv('ALLOWED_HOSTS').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
